4ee4c5af7d9b26d2a49c97ca1bd7d75ae2c22f61
jeanaime-iraguha <jeanaimeiraguha@gmail.com>
2025-04-18 21:34:33 +0200

    first commit

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound(){
+     return(
+          <>
+          <h2>404 || Page Not found</h2>
+  <p>Could not found requested resource</p>        </>
+     )
+}
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,3 +1,5 @@
+import { notFound } from "next/navigation"
+
 export default function ProductLists(){
      return(
           <>

6ff41dc288ef70fb5c9141aecd4f5c25d59b65f5
jeanaime-iraguha <jeanaimeiraguha@gmail.com>
2025-04-18 20:10:17 +0200

    Next js app

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function About(){
+     return(
+          <>
+          <h2 className="text-center mt-5">Welcome to About Page</h2>
+          </>
+     )
+}
---
+++ src/app/profiles/page.tsx
@@ -0,0 +1,7 @@
+export default function Profile(){
+     return(
+          <>
+          <h2 className="text-center mt-12">Welcome to Profile Page</h2>
+          </>
+     )
+}

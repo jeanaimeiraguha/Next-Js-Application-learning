e2f742d3c5b01014fd32291609f440a79b6b6aa8
jeanaime-iraguha <jeanaimeiraguha@gmail.com>
2025-04-18 21:15:34 +0200

    Next js app

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,15 +1,9 @@
-// app/products/[productId]/page.tsx
-
-type ProductDetailsProps = {
-  params: {
-    productId: string;
-  };
-};
-
-export default function ProductDetails({ params }: ProductDetailsProps) {
-  return (
-    <div>
-      <h1>Details about Product {params.productId}</h1>
-    </div>
-  );
+export default function ProductDetails({params}:{
+     params:{productId:string}
+}){
+     return(
+          <>
+          <h1>Details about Products{params.productId}</h1>
+          </>
+     )
 }
